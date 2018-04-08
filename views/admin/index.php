a6d98b5123dcfde8a172b2ed7a1458789bd359f9
noelli <tobiah@noell.li>
2018-04-08 15:36:47 +0200

    added admin listing
    todo csv export

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,6 +1,32 @@
 <?php $view->script('pkenrollment', 'pkenrollment:js/admin.js', 'vue') ?>
-<table id="pkenrollment-table">
-  <tr v-for="entry in entries">
-    <td>{{entry.EBName}}</td>
-  </tr>
+<h1>Anmeldungsübersicht</h1>
+<table id="pkenrollment-table" class="uk-table">
+  <thead>
+    <tr>
+      <th>Erziehungsberechtigter</th>
+      <th>Adresse</th>
+      <th>PLZ, Ort</th>
+      <th>Telefon</th>
+      <th>handy</th>
+      <th>E-mail</th>
+      <th>Ist Erreichbar?</th>
+      <th>Weitere Kontakte</th>
+      <th>Teilnehmer</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="entry in entries">
+      <td>{{entry.EBName}}</td>
+      <td>{{entry.EBAddress}}</td>
+      <td>{{entry.EBLocation}}</td>
+      <td>{{entry.EBTel}}</td>
+      <td>{{entry.EBMobile}}</td>
+      <td>{{entry.EBEmail}}</td>
+      <td>{{entry.EBReachable | trans}}</td>
+      <td>{{entry.EBOtherContacts}}</td>
+      <td>
+        <code v-for="p in entry.participants">{{p.name}}</code>
+      </td>
+    </tr>
+  </tbody>
 </table>
